d1a7169e3277851d678216ee676ae9e91f4b2298
maatheuus <matts14smkd@gmail.com>
2025-08-05 11:30:55 -0300

    feat: update components for show preview image

--- src/components/account/HeaderInformation.tsx
+++ src/components/account/HeaderInformation.tsx
@@ -24,6 +24,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/Tooltip";
+import Image from "next/image";
 import { useRef, useState } from "react";
 import { messagesToDisplayForPremium } from "./utils";
 
@@ -42,6 +43,7 @@ export default function HeaderInformation({
     "https://github.com/shadcn.png"
   );
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isDialogShowImageOpen, setIsDialogShowImageOpen] = useState(false);
   const [isDragging, setIsDragging] = useState(false);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
@@ -103,14 +105,25 @@ export default function HeaderInformation({
   };
 
   return (
-    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+    <Dialog
+      open={isDialogOpen || isDialogShowImageOpen}
+      onOpenChange={() => {
+        if (isEditing) {
+          setIsDialogOpen(!isDialogOpen);
+        } else {
+          setIsDialogShowImageOpen(!isDialogShowImageOpen);
+        }
+      }}
+    >
       <Row className="justify-between w-full relative">
         <Row className="gap-x-4">
           <div className="relative">
-            <Avatar className="size-28 border-4 border-green-500">
-              <AvatarImage src={imagePreview} alt="Avatar do usuário" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+            <DialogTrigger asChild>
+              <Avatar className="size-28 border-4 border-green-500">
+                <AvatarImage src={imagePreview} alt="Avatar do usuário" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DialogTrigger>
             <DialogTrigger asChild>
               {isEditing && (
                 <button className="absolute right-0 top-1.5 z-20 size-7 bg-green-500 rounded-full border-2 border-green-50 cursor-pointer flex items-center justify-center hover:bg-green-600 transition-colors">
@@ -153,79 +166,104 @@ export default function HeaderInformation({
         {!isPremium && <PremiumMemberCard />}
       </Row>
 
-      <DialogContent className="max-w-md">
-        <DialogTitle className="text-green-500 text-center">
-          Alterar foto de perfil
-        </DialogTitle>
-        <DialogDescription className="text-green-200 text-center">
-          Escolha sua mais nova obra de arte
-        </DialogDescription>
+      {!isEditing ? (
+        <DialogContent className="max-w-md">
+          <DialogTitle className="text-green-500 text-center mt-4">
+            Olhe a obra de arte que voce escolheu
+          </DialogTitle>
+          <DialogDescription className="text-green-200 text-center sr-only">
+            foto de perfil do usuário
+          </DialogDescription>
 
-        <div className="space-y-4">
-          <div className="flex justify-center">
-            <Avatar className="size-32 border-4 border-green-500">
-              <AvatarImage src={imagePreview} alt="Preview" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </div>
-
-          <div
-            className={`border-2 border-dashed rounded-lg p-6 text-center transition-colors ${
-              isDragging
-                ? "border-green-500 bg-green-50"
-                : "border-green-600 hover:border-green-200"
-            }`}
-            onDragOver={handleDragOver}
-            onDragLeave={handleDragLeave}
-            onDrop={handleDrop}
-          >
-            <FloppyDiskOutlinedIcon
-              size={32}
-              className="mx-auto text-green-500 mb-2"
+          <div className="w-full rounded-2xl overflow-hidden">
+            <Image
+              src={imagePreview}
+              alt="Preview photo"
+              width={150}
+              height={150}
+              title="Preview photo by user"
+              loading="lazy"
+              unoptimized
+              quality={100}
+              className="w-full h-full object-contain"
             />
-            <Text type={Text.Type.BodyTwo} className="text-green-500 mb-2">
-              Arraste uma imagem aqui ou
-            </Text>
-            <button
-              onClick={() => fileInputRef.current?.click()}
-              className="text-green-500 font-medium hover:text-green-600 underline cursor-pointer"
+          </div>
+        </DialogContent>
+      ) : (
+        <DialogContent className="max-w-md">
+          <DialogTitle className="text-green-500 text-center">
+            Alterar foto de perfil
+          </DialogTitle>
+          <DialogDescription className="text-green-200 text-center">
+            Escolha sua mais nova obra de arte
+          </DialogDescription>
+
+          <div className="space-y-4">
+            <div className="flex justify-center">
+              <Avatar className="size-32 border-4 border-green-500">
+                <AvatarImage src={imagePreview} alt="Preview" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+
+            <div
+              className={`border-2 border-dashed rounded-lg p-6 text-center transition-colors ${
+                isDragging
+                  ? "border-green-500 bg-green-50"
+                  : "border-green-600 hover:border-green-200"
+              }`}
+              onDragOver={handleDragOver}
+              onDragLeave={handleDragLeave}
+              onDrop={handleDrop}
             >
-              clique para selecionar
-            </button>
-            <input
-              ref={fileInputRef}
-              type="file"
-              accept="image/*"
-              onChange={handleFileInputChange}
-              className="hidden"
-            />
-            <Text type={Text.Type.BodyFour} className="text-green-200 mt-2">
-              PNG, JPG ou JPEG até 5MB
-            </Text>
-          </div>
-
-          {/* Botões de ação */}
-          <div className="flex gap-2">
-            {selectedImage && (
-              <Button
-                variant="text"
-                onClick={handleImageRemove}
-                className="flex-1 text-red-500 hover:text-red-600 cursor-pointer"
+              <FloppyDiskOutlinedIcon
+                size={32}
+                className="mx-auto text-green-500 mb-2"
+              />
+              <Text type={Text.Type.BodyTwo} className="text-green-500 mb-2">
+                Arraste uma imagem aqui ou
+              </Text>
+              <button
+                onClick={() => fileInputRef.current?.click()}
+                className="text-green-500 font-medium hover:text-green-600 underline cursor-pointer"
               >
-                <TrashOutlinedIcon size={16} className="mr-2" />
-                Remover
+                clique para selecionar
+              </button>
+              <input
+                ref={fileInputRef}
+                type="file"
+                accept="image/*"
+                onChange={handleFileInputChange}
+                className="hidden"
+              />
+              <Text type={Text.Type.BodyFour} className="text-green-200 mt-2">
+                PNG, JPG ou JPEG até 5MB
+              </Text>
+            </div>
+
+            {/* Botões de ação */}
+            <div className="flex gap-2">
+              {selectedImage && (
+                <Button
+                  variant="text"
+                  onClick={handleImageRemove}
+                  className="flex-1 text-red-500 hover:text-red-600 cursor-pointer"
+                >
+                  <TrashOutlinedIcon size={16} className="mr-2" />
+                  Remover
+                </Button>
+              )}
+              <Button
+                onClick={handleImageSave}
+                disabled={!selectedImage}
+                className="flex-1 bg-green-500 text-white hover:bg-green-600 disabled:opacity-50  cursor-pointer"
+              >
+                Salvar foto
               </Button>
-            )}
-            <Button
-              onClick={handleImageSave}
-              disabled={!selectedImage}
-              className="flex-1 bg-green-500 text-white hover:bg-green-600 disabled:opacity-50  cursor-pointer"
-            >
-              Salvar foto
-            </Button>
+            </div>
           </div>
-        </div>
-      </DialogContent>
+        </DialogContent>
+      )}
     </Dialog>
   );
 }
