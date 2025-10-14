8f1ce296711860bc1cf74d07498068eb0e27caf4
maatheuus <matts14smkd@gmail.com>
2025-10-14 20:55:17 -0300

    fix: error build

--- src/components/auth/ui/Signup/CodeChecked.tsx
+++ src/components/auth/ui/Signup/CodeChecked.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
 import { ArrowCircleRightOutlinedIcon } from "@/components/icons";
 import Button from "@/components/ui/Button";
 import Col from "@/components/ui/Layout/Helpers/Col";
-import { useStepStore } from "@/hooks/auth/signupFlow/setLocalData";
+// import { useStepStore } from "@/hooks/auth/signupFlow/setLocalData";
 
 export default function CodeChecked() {
-  const { nextStep, setStep } = useStepStore();
+  // const { nextStep, setStep } = useStepStore();
 
   return (
     <Col className="items-center gap-2 px-5 py-4">
@@ -14,11 +14,11 @@ export default function CodeChecked() {
         type="submit"
         rightIcon={<ArrowCircleRightOutlinedIcon size={24} />}
         text="Seguinte"
-        className="font-semibold w-full sm:max-w-80 hover:[&_svg]:translate-x-1.5 hover:[&_svg]:transition-all hover:[&_svg]:duration-300"
-        onClick={() => {
-          setStep("verification");
-          nextStep();
-        }}
+        className="w-full font-semibold sm:max-w-80 hover:[&_svg]:translate-x-1.5 hover:[&_svg]:transition-all hover:[&_svg]:duration-300"
+        // onClick={() => {
+        //   setStep("verification");
+        //   nextStep();
+        // }}
       />
     </Col>
   );
