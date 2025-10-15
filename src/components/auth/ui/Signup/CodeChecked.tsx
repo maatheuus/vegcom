6cfce1702774b601809b0864c66f345a376e37e4
maatheuus <matts14smkd@gmail.com>
2025-10-14 21:05:45 -0300

    fix: error build

--- src/components/auth/ui/Signup/CodeChecked.tsx
+++ src/components/auth/ui/Signup/CodeChecked.tsx
@@ -1,13 +1,8 @@
-"use client";
-
 import { ArrowCircleRightOutlinedIcon } from "@/components/icons";
 import Button from "@/components/ui/Button";
 import Col from "@/components/ui/Layout/Helpers/Col";
-// import { useStepStore } from "@/hooks/auth/signupFlow/setLocalData";
 
 export default function CodeChecked() {
-  // const { nextStep, setStep } = useStepStore();
-
   return (
     <Col className="items-center gap-2 px-5 py-4">
       <Button.Icon
