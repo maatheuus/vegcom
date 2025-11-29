022b23e0558305d5a58012ae9634c1840444b6e6
maatheuus <matts14smkd@gmail.com>
2025-11-29 17:28:20 -0300

    adding analytics from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { lora, maitree, montserrat, rancho } from "@/assets/fonts";
 import SmoothScroll from "@/shared/components/ui/SmoothScroll";
 import QueryClientWrapper from "@/shared/tanstack/QueryClientWrapper";
 import { Toaster } from "@/shared/ui/toaster";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import "./global.css";
@@ -37,6 +38,7 @@ export default function RootLayout({
           <Toaster />
         </QueryClientWrapper>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
