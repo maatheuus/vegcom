dde8ee3937f5c4e4f084b1216aa6de55a1fe5a31
Matheus <matts14smkd@gmail.com>
2026-03-20 22:29:28 -0300

    Update page.tsx to change container class and add BackButton component

--- app/(public)/user/[id]/page.tsx
+++ app/(public)/user/[id]/page.tsx
@@ -1,11 +1,28 @@
-import BackButton from 'src/features/recipe-details/BackButton';
+import { getUserDetails } from "@/features/userProfile/api/userApi";
+import BackButton from "@/features/recipe-details/BackButton";
+import { UserProfileHeader } from "@/features/userProfile/components/UserProfileHeader";
+import UserProfileTabsClient from "@/features/userProfile/components/UserProfileTabsClient";
+import Layout from "@/shared/ui/Layout/";
+import { notFound } from "next/navigation";
 
-export default function Page({ user }) {
+interface Props {
+  params: Promise<{ id: string }>;
+}
+
+export default async function Page({ params }: Props) {
+  const { id } = await params;
+  const response = await getUserDetails(Number(id)).catch(() => null);
+
+  if (!response?.data) notFound();
+
+  const user = response.data;
   return (
-    <div className="body__container-lg flex flex-col gap-y-5 py-6">
-      <BackButton />
-      <UserProfileHeader user={user} />
-      {/* other content */} 
-    </div>
+    <Layout.Default className="style-scrollbar">
+      <div className="body__container-lg flex flex-col gap-y-5 py-6">
+        <BackButton />
+        <UserProfileHeader user={user} />
+        <UserProfileTabsClient user={user} />
+      </div>
+    </Layout.Default>
   );
 }
