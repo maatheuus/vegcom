c75cec6f54e3fe5d7fa62eda67c3455df238fbad
maatheuus <matts14smkd@gmail.com>
2026-04-08 18:46:54 -0300

    refactor: ajustar espaçamento nos botões do DesktopMenu

--- src/features/community/components/Menu/DesktopMenu.tsx
+++ src/features/community/components/Menu/DesktopMenu.tsx
@@ -73,7 +73,7 @@ export default function DesktopMenu({
         {isLoggedIn && (
           <Link href={upgradeLink.href} className="group/plant contents">
             <Row
-              className={`${cls.baseButton} items-center justify-center rounded-lg bg-green-200 p-2 text-green-50 group-hover/plant:bg-green-100`}
+              className={`${cls.baseButton} items-center justify-center rounded-lg bg-green-200 p-1 text-green-50 group-hover/plant:bg-green-100`}
             >
               <upgradeLink.icon
                 size={20}
@@ -91,7 +91,7 @@ export default function DesktopMenu({
         )}
 
         <DropdownMenu>
-          <DropdownMenuTrigger className="group/user flex cursor-pointer items-center gap-x-2 rounded-lg border-0 bg-green-100 p-2 transition-colors duration-300 outline-none hover:bg-green-200">
+          <DropdownMenuTrigger className="group/user flex cursor-pointer items-center gap-x-2 rounded-lg border-0 bg-green-100 p-1 transition-colors duration-300 outline-none hover:bg-green-200">
             <UserCircleIcon
               size={20}
               className="size-5 text-green-200 group-hover/user:text-green-50"
